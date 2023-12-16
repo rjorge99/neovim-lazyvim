023ac7da1a2f3dbbdb389d4d3ba7d097d7d57f4f
Jorge Reyes <rjorge99@hotmail.com>
2023-12-16 17:02:03 -0600

    Se agrega colores de tailwind

---
+++ lua/user/plugins/nvim-colorizer.lua
@@ -0,0 +1,12 @@
+return {
+  "NvChad/nvim-colorizer.lua",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local colorizer = require("colorizer")
+    colorizer.setup({
+      user_default_options = {
+        tailwind = true,
+      },
+    })
+  end,
+}
