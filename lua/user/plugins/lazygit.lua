2056c92662ba1b3ae92ce3929db2228601092bf6
Jorge Reyes <rjorge99@hotmail.com>
2023-12-19 16:15:34 -0600

    Se agrega keymap para lazygit

--- lua/user/plugins/lazygit.lua
+++ lua/user/plugins/lazygit.lua
@@ -3,4 +3,10 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
+
+  config = function()
+    local keymap = vim.keymap.set
+
+    keymap("n", "<leader>lg", "<cmd>LazyGit<cr>", { desc = "LazyGit" })
+  end,
 }
