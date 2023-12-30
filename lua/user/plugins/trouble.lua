2dbf6550d1db3d22365006468d7a7f8cb0beb553
Jorge Reyes <rjorge99@hotmail.com>
2023-12-30 09:29:28 -0600

    Se agrega shortcut para touble

--- lua/user/plugins/trouble.lua
+++ lua/user/plugins/trouble.lua
@@ -1,9 +1,9 @@
 return {
   "folke/trouble.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
+  config = function()
+    local keymap = vim.keymap
+
+    keymap.set("n", "<leader>t", "<cmd>TroubleToggle<cr>", { desc = "Trouble Toggle" })
+  end,
 }
