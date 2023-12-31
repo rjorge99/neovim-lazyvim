56aa376f3f5b8a4a9c1fc48920b60f249ecb6468
Jorge Reyes <rjorge99@hotmail.com>
2023-12-31 12:26:27 -0600

    Oil shows hidden files

--- lua/user/plugins/oil-nvim.lua
+++ lua/user/plugins/oil-nvim.lua
@@ -4,7 +4,11 @@ return {
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("oil").setup()
+    require("oil").setup({
+      view_options = {
+        show_hidden = true,
+      },
+    })
 
     vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
   end,
